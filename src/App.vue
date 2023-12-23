a398c5f117befa745fe13f3f32edfc5a3f01b9ca
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-12-23 16:25:14 +0100

    second time

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :id="$style.app">
+    <ApartmentsList :items="apartments">
+      <template v-slot:apartment="{ apartment }">
+        <ApartmentsItem
+          :key="apartment.id"
+          :descr="apartment.descr"
+          :rating="apartment.rating"
+          :price="apartment.price"
+          :imgSrc="apartment.imgUrl"
+          class="apartments-list__item"
+        />
+      </template>
+      <template v-slot:title>New title</template>
+      <template v-slot:description>New description</template>
+    </ApartmentsList>
+  </div>
+</template>
+
+<script>
+import ApartmentsItem from './components/apartments/ApartmentsItem.vue';
+import ApartmentsList from './components/apartments/ApartmentsList.vue';
+import apartments from './components/apartments/apartments';
+
+export default {
+  name: 'App',
+  components: {
+    ApartmentsItem,
+    ApartmentsList,
+  },
+  data() {
+    return {
+      apartments
+    }
+  }
+}
+</script>
+
+<style module>
+#app {
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/* .content {
+  flex-grow: 1;
+} */
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button v-on="$listeners" v-bind:type="type" class="btn btn--outlined">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'MyButton',
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+            },
+            outlined: {
+                type: Boolean,
+                default: false
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+.btn {
+    display: inline-block;
+    border: 1px solid transparent;
+    padding: 10px 20px;
+    margin: 5px 10px;
+    font-size: 18px;
+    background: #FF6620;
+    color: #fff; 
+    min-width: 220px;
+    padding: 8px 15px;
+
+    &--outlined {
+        background: none;
+        border: 1px solid #FF6620;
+        color: #FF6620;
+    }
+}
+
+</style>
